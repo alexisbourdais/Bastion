c76e35758b7912f422c94cd0aa4b3609bca5cced
alexisbourdais <108393526+alexisbourdais@users.noreply.github.com>
2024-10-13 19:15:10 +0200

    Add files via upload

--- Launcher_Contams.sh
+++ Launcher_Contams.sh
@@ -1,5 +1,9 @@
 #!  /usr/bin/bash
 
-. /local/env/envnextflow-23.10.0.sh
+#. /local/env/envnextflow-23.10.0.sh
 
-nextflow run Contams.nf -profile slurm,conda
+nextflow run Contams.nf \
+-profile standard,conda \
+--workflow setup \
+--singularity "-B /home:/home" \
+--setKraken2
