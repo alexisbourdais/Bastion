91fbee06807971ccfbc7688514467d52c69000fd
alexisbourdais <108393526+alexisbourdais@users.noreply.github.com>
2025-02-03 17:02:25 +0100

    Update Launcher_Bastion_analysis.sh

--- Launcher_Bastion_analysis.sh
+++ Launcher_Bastion_analysis.sh
@@ -9,8 +9,6 @@ nextflow run Bastion.nf \
 --db_busco /scratch/abourdais/Database/busco_downloads/ \
 --db_kraken2 /scratch/abourdais/Database/kraken2_db/ \
 --db_gtdbtk /scratch/abourdais/Database/gtdbtk_db/release220/ \
---db_omark /projects/bipaa/ref/OMA/LUCA.h5 \
 --db_kmerfinder /scratch/abourdais/Database/kmerfinder_db/
 
-#--assemblyDir "/projects/holoe2plant/Alexis_Bacteria/5_ASSEMBLY_125/" \
 #--format "fna" \
